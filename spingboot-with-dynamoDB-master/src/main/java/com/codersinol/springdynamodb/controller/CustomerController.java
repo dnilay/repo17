3745e3cd6e32ac3ddb1020679f9cafa301968aec
Nilay <54245048+dnilay@users.noreply.github.com>
2021-09-08 16:40:27 +0530

    Add files via upload

---
+++ spingboot-with-dynamoDB-master/src/main/java/com/codersinol/springdynamodb/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.codersinol.springdynamodb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.codersinol.springdynamodb.dao.CustomerDao;
+import com.codersinol.springdynamodb.model.Customer;
+
+@RestController
+public class CustomerController {
+    
+    private @Autowired CustomerDao customerDao;
+    
+    @RequestMapping(value = "/users", produces = {"application/json"}, method = RequestMethod.GET)
+    public Customer getCustomer() {
+       Customer newCustomer = new Customer();
+       newCustomer.setCustomerId("kkk");
+        newCustomer.setName("Marry Gold");
+        newCustomer.setEmail("marry@email.com");
+        Customer cus = customerDao.save(newCustomer);
+        Customer c = customerDao.getCustomer("kkk");
+        return c;
+    }
+
+
+}
